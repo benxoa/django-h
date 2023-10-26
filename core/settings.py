536e3217a36bf9ce5981016798b153ba5ad0b993
benxoa <benxoax@gmail.com>
2023-10-26 22:05:34 +0500

    FinalcsfrtjFd

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,9 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config()
+
+DATABASES = {'default': dj_database_url.config()}
+
 
 
 # Password validation
--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
+    if request.method == "POST":
+        image = request.FILES.get('image')
+
+        Image.objects.create(image=image)
+
     return render(request, 'home.html')
